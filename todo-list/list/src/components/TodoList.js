a980e4a69e27a60ee98d78e2ebdb92dbf9de4d6d
ape <kgonzale@cbu.edu>
2018-11-03 15:11:47 -0500

    addition works

--- todo-list/list/src/components/TodoList.js
+++ todo-list/list/src/components/TodoList.js
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 
 class TodoList extends Component {
   render() {
-    return null;
+    const listArray = [];
+    for (let i = 0; i < this.props.todos.length; i++) {
+      const deleteFunction = i => {
+        this.props.onDeleteTodo(i);
+      };
+
+      listArray.push(this.props.todos[i]);
+    }
+
+    return (
+      <div>
+        <button onClick={this.deleteFunction}>X</button>
+        <div>{listArray}</div>
+      </div>
+    );
   }
 }
 
