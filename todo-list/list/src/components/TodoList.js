9f897219673d8785c7c381e3ffb8586915183102
ape <kgonzale@cbu.edu>
2018-11-06 13:46:28 -0600

    .......

--- todo-list/list/src/components/TodoList.js
+++ todo-list/list/src/components/TodoList.js
@@ -2,19 +2,26 @@ import React, { Component } from "react";
 
 class TodoList extends Component {
   render() {
-    const listArray = [];
-    for (let i = 0; i < this.props.todos.length; i++) {
-      const deleteFunction = i => {
-        this.props.onDeleteTodo(i);
-      };
+    // const listArray = [];
+    // for (let i = 0; i < this.props.todos.length; i++) {
+    //   const deleteFunction = i => {
+    //     this.props.onDeleteTodo(i);
+    //   };
 
-      listArray.push(this.props.todos[i]);
-    }
+    //   listArray.push(this.props.todos[i]);
+    // }
+
+    // console.log(listArray);
+
+    const copyTodos = this.props.todos.slice();
+    const array = copyTodos.map((item, index) => {
+      return <div key={index}>{item}</div>;
+    });
 
     return (
       <div>
         <button onClick={this.deleteFunction}>X</button>
-        <div>{listArray}</div>
+        <div>{array}</div>
       </div>
     );
   }
