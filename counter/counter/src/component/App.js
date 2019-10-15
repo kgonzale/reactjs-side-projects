1c75bb55cf4e2136b2a471b6da4f7f48e53f31fe
unknown <kgonzale@cbu.edu>
2019-10-14 19:29:09 -0500

    yeetus

--- counter/counter/src/component/App.js
+++ counter/counter/src/component/App.js
@@ -1,25 +1,15 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
 
-export default class App extends Component {
-  state = {
-    counter: 0
-  };
+const App = () => {
+  const [number, setNumber] = useState(0);
 
-  handleOnClickInc = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  handleOnClickDec = () => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleOnClickInc}>+</button>
-        <button onClick={this.handleOnClickDec}>-</button>
-        {this.state.counter}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <button onClick={() => setNumber + 1}>+</button>
+      <button onClick={() => setNumber - 1}>-</button>
+      {number}
+    </div>
+  );
 }
+
+export default App;
